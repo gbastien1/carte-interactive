e798ff0b59deb86d5422724901d707dc4d181e42
gbastien1 <gabrielle.bastien1@gmail.com>
2016-05-01 21:18:31 -0400

    enabled carte_interactive app and created templates directories hierarchy, now works

--- international/carte_interactive/views.py
+++ international/carte_interactive/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+	template_name = "carte_interactive/index.html"
--- international/international/settings.py
+++ international/international/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carte_interactive'
 ]
 
 MIDDLEWARE_CLASSES = [
