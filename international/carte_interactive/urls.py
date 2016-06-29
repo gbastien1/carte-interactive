addc25b8f1505310ecad46d5e7d818b373670190
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-06-29 09:52:23 -0400

    added rewrite and reinit urls, and removed as_view() on uploadExcelView because not class based anymore

--- international/carte_interactive/urls.py
+++ international/carte_interactive/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
 	url(r'^$', views.LoginView.as_view(), name='login'),
 	url(r'^carte/logout/$', views.LogoutView.as_view(), name='logout'),
 	url(r'^carte/$', views.CardView.as_view(), name='carte'),
-	url(r'^carte/upload/$', views.UploadExcelView.as_view(), name='upload_excel'),
+	# url(r'^carte/upload/$', views.UploadExcelView, name='upload_excel'),
+	url(r'^carte/rewrite/$', views.RewriteExcelView, name='rewrite_excel'),
+	url(r'^carte/reinit/$', views.ReinitVisitsView, name='reinit_visits'),
 	url(r'^carte/edit/$', views.EditerEcole, name='edit_ecole'),
 	url(r'^carte/savePosition/$', views.SavePositionView, name='save_position')
 ]
