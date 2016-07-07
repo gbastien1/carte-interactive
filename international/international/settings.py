41e1fa6bb130c12a4c8521a532046e8ed00382be
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-07-07 10:49:46 -0400

    removed data related to sqlite and changed sqlite settings to postgresql settings

--- international/international/settings.py
+++ international/international/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'international.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gbastien1',
+        'USER': 'gbastien1',
+        'PASSWORD': 'Basg20549403$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,5 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'carte_interactive', 'static', 'carte_interactive', 'data')
