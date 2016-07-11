686ec22a56716687fd5657b0469bf4cf997ec525
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-07-11 17:24:44 -0400

    fixed small error in settings

--- international/settings.py
+++ international/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
