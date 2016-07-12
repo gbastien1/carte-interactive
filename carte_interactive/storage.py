06823e61d8aea5715c191b94c8649549c6c0f405
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-07-12 08:34:46 -0400

    try to fix problem with path in storage.py for heroku

--- carte_interactive/storage.py
+++ carte_interactive/storage.py
@@ -5,10 +5,10 @@ import os
 class OverwriteStorage(FileSystemStorage):
 
     def get_available_name(self, name, max_length):
-        name = name.split('\\')[1]
+        # name = name.split('\\')[1]
         # If the filename already exists, remove it as if it was a true file system
         if self.exists(name):
-            path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, name)).replace('\\', '/')
+            path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, name))#.replace('\\', '/')
             try:
                 os.remove(path)
             except OSError as e:
