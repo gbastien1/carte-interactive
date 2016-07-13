6bb84d5b07ca9fcc4b58924235f490b47a902f1e
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-07-13 10:50:22 -0400

    modified search to get data.json only once

--- carte_interactive/static/carte_interactive/js/interfaceCallbacks.js
+++ carte_interactive/static/carte_interactive/js/interfaceCallbacks.js
@@ -149,8 +149,14 @@ $("#search_input").keyup(function (e) {
 $('#search_input').on('input', function() {
     // if used on small viewport, search results hide 
     // search bar and it is inconvenient
-    if ($(window).width() > 420){  
-        search();
+    if ($(window).width() > 420){ 
+        if (!data_json) {
+            var json_url = "../../static/carte_interactive/json/data.json";
+            $.getJSON(json_url, function(ecoles) {
+                data_json = ecoles;
+                search(data_json);
+            });
+        } else search(data_json);
     }
 });
 
@@ -266,36 +272,32 @@ function filterMarkers(closeSidebar) {
 function search() {
     var input_text = $("#search_input").val().toLowerCase();
     if(input_text) {
-        //var json_url = "/static/carte_interactive/json/data.json";
+        var json_url = "../../static/carte_interactive/json/data.json";
         var ecole_data = [];
-        // get Json data
-        //$.getJSON(json_url, function(ecoles) {
-            // create a string containing all the information that could be searched
-            // make array from every Ecole treated
-            ecoles = json_db_data;
-            ecoles.forEach(function(e) {
-                var string_data = e.pk + ',' + e.fields.nom + ',' + e.fields.programmes + ',' + e.fields.type + ',' + e.fields.ville;
-                string_data = string_data.toLowerCase();
-                ecole_data.push(string_data);
-            });
-            var ecole_results = [];
-            // go through each marker, if one marker has the typed information, set it visible
-            markers.forEach(function(m) {
-                ecole_data.forEach(function(e, index) {
-                    if (e.split(',')[0] == m.pk) {
-                        ecole_from_json = e;
-                        ecole_index = index;
-                    }
-                });
-                m.setVisible(false);
-                if (ecole_from_json.indexOf(input_text) != -1) {
-                    m.setVisible(true);
-                    ecole_results.push(ecoles[ecole_index].fields);
+        // create a string containing all the information that could be searched
+        // make array from every Ecole treated
+        ecoles.forEach(function(e) {
+            var string_data = e.pk + ',' + e.fields.nom + ',' + e.fields.programmes + ',' + e.fields.type + ',' + e.fields.ville;
+            string_data = string_data.toLowerCase();
+            ecole_data.push(string_data);
+        });
+        var ecole_results = [];
+        // go through each marker, if one marker has the typed information, set it visible
+        markers.forEach(function(m) {
+            ecole_data.forEach(function(e, index) {
+                if (e.split(',')[0] == m.pk) {
+                    ecole_from_json = e;
+                    ecole_index = index;
                 }
             });
-            // show a result list with the gathered corresponding markers
-            showResultsList(ecole_results);
-        //});
+            m.setVisible(false);
+            if (ecole_from_json.indexOf(input_text) != -1) {
+                m.setVisible(true);
+                ecole_results.push(ecoles[ecole_index].fields);
+            }
+        });
+        // show a result list with the gathered corresponding markers
+        showResultsList(ecole_results);
     }
     //if nothing was entered in search bar, show all markers
     else {
